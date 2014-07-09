c762ad7d49d10c95ae8b0ad2bdcf7cf067b41b57
Ostap Horbach <mail.ostapa@gmail.com>
2014-07-09 19:07:14 +0300

    Initial commit

---
+++ FlickGallery/FlickGallery/FGAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  FGAppDelegate.h
+//  FlickGallery
+//
+//  Created by Ostap Horbach on 7/9/14.
+//  Copyright (c) 2014 Ostap Horbach. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FGAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ FlickGallery/FlickGallery/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  FlickGallery
+//
+//  Created by Ostap Horbach on 7/9/14.
+//  Copyright (c) 2014 Ostap Horbach. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "FGAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([FGAppDelegate class]));
+    }
+}
